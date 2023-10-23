eadec5dfaf3726867628644202791a2e470e2248
Your Name <nhatquan.nvx@gmail.com>
2023-10-23 13:33:07 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import {useState} from 'react'
+
+
 
 function App() {
+
+  const [work, setWork] = useState('')
+
+
   return (
-    <div className="App">
-      React app
+    <div className="flex  gap-8 h-screen justify-center border border-red-500 items-center">
+     <input type="text" 
+     className="outline-none w-[400px] border border-blue-600 px-4 py-2"
+      value={work}
+      onChange={e=> setWork(e.target.value )} 
+    />
+      <button
+      type="button"
+      className="outline-none px-4 py-2 rounded-md text-white  bg-blue-500"
+    >
+      Add
+    </button>
+    
     </div>
   );
 }
