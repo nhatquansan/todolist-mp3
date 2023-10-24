d844886f9a2e5fd6d5c376aea89623201c7ca1e3
Your Name <nhatquan.nvx@gmail.com>
2023-10-24 18:23:27 +0700

    v4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import {useState} from 'react'
-
-
+import { ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [work, setWork] = useState('')
   const [todos, setTodos] = useState([])
   const handleAdd = () => {
-      setTodos(prev => [...prev, work])
+    if(todos?.some(item => item.id ==  work?.replace(/\s/g,'')))
+    {
+      toast.warn('Công việc đã được thêm trước đó')
+    }
+    else {
+      setTodos(prev => [...prev, {id : work?.replace(/\s/g,''),job: work}])
       setWork('')
+    }  
+  }
+
+  const handleDeleteJob = (id) => {
+      setTodos(prev => prev.filter(item => item.id != id))
   }
 
 
   return (
+  <>
     <div className="flex flex-col gap-8 h-screen justify-center border border-red-500 items-center">
     <div className="flex gap-8">
     <input type="text" 
@@ -31,17 +42,32 @@ function App() {
     <div>
       <h3 className='font-bold text-xl'>Content: </h3>
       <ul>
-          {todos?.map((item,index) => {
+          {todos?.map((item) => {
             return (
-              <li key={index}>
-                {item}
+              <li key={item.id} className='flex gap-10 items-center'>
+                <span className='my-2'>{item.job} </span>
+                <span onClick={()=>handleDeleteJob(item.id)} className='my-2 cursor-pointer p-2'>X</span>
               </li>
             )
           })}
       </ul>
     </div>
-    
     </div>
+    <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    />
+    {/* Same as */}
+    <ToastContainer />
+  </>
   );
 }
 
